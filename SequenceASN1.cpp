9613178953798f7e36c82be968c65f823888cf0e
zhanghao <zhangh58e@visionvera.com>
2022-10-13 23:22:03 +0800

    1

---
+++ SequenceASN1.cpp
@@ -0,0 +1,33 @@
+#include "SequenceASN1.h"
+
+SequenceASN1::SequenceASN1(/* args */)
+{
+}
+
+int SequenceASN1::writeHeadNode(){
+
+}
+
+int SequenceASN1::writeNextNode(){
+
+}
+
+int SequenceASN1::readHeadNode(){
+
+}
+
+int SequenceASN1::readNextNode(){
+
+}
+
+int SequenceASN1::packSequence(){
+
+}
+
+int SequenceASN1::unpackSequence(){
+
+}
+
+void SequenceASN1::freeSequence(){
+
+}
